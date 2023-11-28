533ecf88e13057527724c98734e2b5e2f049e8ab
JustbeV <138653607+JustbeV@users.noreply.github.com>
2023-11-28 16:43:19 +0800

    Add files via upload

---
+++ CC103FirstAss.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cc103firstass;
+
+/**
+ *
+ * @author Administrator
+ */
+public class CC103FirstAss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //num 1:
+        int num=10;
+        
+        for(int i=1; i<=num; i++){
+            
+            System.out.print(i);
+          if(i<num){
+              System.out.print("-");
+          }  
+          
+            
+        }
+        
+        //num 2:
+//     int num=30;
+//     int total=0;
+//     
+//     for(int i=0; i <=num; i++){
+//         System.out.print(" "+ i);
+//        total +=i;
+//     }
+//        System.out.println();
+//        System.out.println("The total is:"+ total);
+
+        //num 3:
+//        int row=5;
+//        int col=5;
+//        for(int i=0; i<row; i++){
+//            for(int j=0; j<i; j++){  
+//                System.out.print("* ");
+//            } 
+//            System.out.println("* ");
+//           
+//        }
+
+        //num 4:
+//        int row=5;
+//        //int col=5;
+//        for(int i=0; i<=row; i++){
+//            for(int j=0; j<=i-1; j++){  
+//                System.out.print("* ");
+//            }          
+//            System.out.println();
+//        }
+//        for(int i=row; i>=1; i--){
+//            for(int j=1; j<=i; j++){  
+//                System.out.print("* ");
+//            } 
+//            System.out.println();
+//
+//    }
+//    }
+    }
+}
